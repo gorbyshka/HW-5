149d979ef309c11070405c9d7641dd7d8608a28e
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-11-27 08:26:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const firstString = prompt('Введіть 1 рядок');
+const secondString = prompt('Введіть 2 рядок');
+const thirdString = prompt('Введіть 3 рядок');
+
+const firstConversion = String(firstString);
+const secondConversion = String(secondString);
+const thirdConversion = String(thirdString);
+
+const resultString = firstConversion + ', ' + secondConversion + ', ' + thirdConversion + '.';
+
+alert(resultString);
+
+const firstNumber = prompt('Введіть перше число');
+const secondNumber = prompt('Введіть друге число');
+const thirdNumber = prompt('Введіть третє число');
+const fourthNumber = prompt('Введіть четверте число');
+const fifthNumber = prompt('Введіть п\'яте число');
+
+const firstnumConversion = Number(firstNumber);
+const secondnumConversion = Number(secondNumber);
+const thirdnumConversion = Number(thirdNumber);
+const fourthnumConversion = Number(fourthNumber);
+const fifthnumConversion = Number(fifthNumber);
+
+alert(firstnumConversion + ' ' + secondnumConversion + ' ' + thirdnumConversion + ' ' + fourthnumConversion + ' ' + fifthnumConversion);
